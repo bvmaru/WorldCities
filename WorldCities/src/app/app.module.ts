d47b7fc1d3238979abff54d628e149c0d40477df
Marcel <bvmaru@proton.me>
2024-02-06 14:08:14 +0100

    Add project files.

---
+++ WorldCities/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CitiesComponent } from './cities/cities.component';
+import { CityEditComponent } from './cities/city-edit.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CountryEditComponent } from './countries/country-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    CitiesComponent,
+    CityEditComponent,
+    CountriesComponent,
+    CountryEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WorldCitiesAPI/Controllers/CountriesController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CountriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<ApiResult<Country>>> GetCountries(
+            int pageIndex = 0,
+            int pageSize = 10,
+            string? sortColumn = null,
+            string? sortOrder = null,
+            string? filterColumn = null,
+            string? filterQuery = null)
+        {
+            return await ApiResult<Country>.CreateAsync(
+            _context.Countries.AsNoTracking(),
+            pageIndex,
+            pageSize,
+            sortColumn,
+            sortOrder,
+            filterColumn,
+            filterQuery);
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            if (_context.Countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+            if (_context.Countries == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Countries'  is null.");
+            }
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            if (_context.Countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return (_context.Countries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        [HttpPost]
+        [Route("IsDupeField")]
+        public bool IsDupeField(
+            int countryId,
+            string fieldName,
+            string fieldValue)
+        {
+            //default
+            //switch (fieldName)
+            //{
+            //    case "name":
+            //        return _context.Countries.Any(
+            //        c => c.Name == fieldValue && c.Id != countryId);
+            //    case "iso2":
+            //        return _context.Countries.Any(
+            //        c => c.ISO2 == fieldValue && c.Id != countryId);
+            //    case "iso3":
+            //        return _context.Countries.Any(
+            //        c => c.ISO3 == fieldValue && c.Id != countryId);
+            //    default:
+            //        return false;
+            //}
+
+            // Alternative approach (using System.Linq.Dynamic.Core)
+            return (ApiResult<Country>.IsValidProperty(fieldName, true))
+            ? _context.Countries.Any(
+            string.Format("{0} == @0 && Id != @1", fieldName),
+            fieldValue,
+            countryId)
+            : false;
+        }
+    }
+}
